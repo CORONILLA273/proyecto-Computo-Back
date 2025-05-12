fc9722cf493ce149b22c8618d3201d11c5fb6aa8
AngelEUrbinaN <ae.urbinanavarrete@ugto.mx>
2025-05-12 15:27:28 -0600

    [AGREGADO] Creay usuario e iniciar sesión

---
+++ src/interfaces/IUserRepository.js
@@ -0,0 +1,24 @@
+export default class IUserRepository {
+
+    create(user) {
+        throw new Error('Metodo no implementado')
+    }
+    update(id,  updateData) {
+        throw new Error('Metodo no implementado')
+    }
+    delete(id) {
+        throw new Error('Metodo no implementado')
+    }
+    getAll() {
+        throw new Error('Metodo no implementado')
+    }
+    findByFullname(fullName) {
+        throw new Error('Metodo no implementado')
+    }
+    findByEmail(email) {
+        throw new Error('Metodo no implementado')
+    }
+    getById(id) {
+        throw new Error('Metodo no implementado')
+    }
+}
---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+export class User {
+    constructor(fullName, email, password, businessName, industry, domain, product, type, storage_size) {
+        this.fullName = fullName
+        this.email = email
+        this.password = password
+        this.businessName = businessName
+        this.industry = industry
+        this.domain = domain
+        this.product = product
+        this.type = type
+        this.storage_size = storage_size
+    }
+}
