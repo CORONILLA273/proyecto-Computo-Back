716cc119904f951a65738610524532d37a25dc15
Coronilla <e.coronillahernandez@ugto.com>
2025-06-05 21:57:35 -0600

    Cambio en Sale

--- src/models/Sale.js
+++ src/models/Sale.js
@@ -1,7 +1,8 @@
 export class Sale {
-    constructor({ orderedDate, customerId, status}){
+    constructor({ orderedDate, customerId, total, status}){
         this.orderedDate = orderedDate
         this.customerId = customerId
+        this.total = total
         this.status = status
     }
 }
